35129ee94a63dc60e9b8098c1e0a08cd61c7429c
MatPrayer <zmattia2005@gmail.com>
2026-05-09 12:22:58 +0200

    feat: add auth/register endpoint #1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -20,22 +20,4 @@ router.get('/', async function(req, res, next) {
   }
 });
 
-/* POST user */
-router.post('/users', async function(req, res, next) {
-  try {
-    const db = await connectDB();
-
-    const result = await db
-      .collection('users')
-      .insertOne({
-        name: req.body.name
-      });
-
-    res.json(result);
-
-  } catch (err) {
-    next(err);
-  }
-});
-
 module.exports = router;
