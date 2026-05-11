e9367446f7bbd2c20312911885cdbfa47238415c
Mirko Lana <lana.mirko@icloud.com>
2026-05-11 20:31:55 +0200

    feat: add `Climber` user

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
+const baseUserTransform = (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+};
+
+// --- USER ---
 const userSchema = new mongoose.Schema(
     {
         email: { type: String, required: true, unique: true, lowercase: true },
@@ -15,7 +23,13 @@ const userSchema = new mongoose.Schema(
         googleId: { type: String, unique: true, sparse: true },
         authMethods: [{ type: String, enum: ["local", "google"] }],
     },
-    { timestamps: true },
+    {
+        timestamps: true,
+        discriminatorKey: "userType",
+        toJSON: {
+            transform: baseUserTransform,
+        },
+    },
 );
 
 userSchema.pre("save", async function () {
@@ -30,4 +44,38 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+// --- CLIMBER ---
+const climberSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        surname: { type: String, required: true, trim: true },
+        birthdate: { type: Date, required: true },
+        bio: {
+            type: String,
+            default: "",
+            trim: true,
+            maxlength: [200, "Bio cannot exceed 200 characters"],
+        },
+        wallet: {
+            type: Number,
+            default: 0,
+            min: [0, "Wallet balance cannot be negative"],
+        },
+    },
+    {
+        toJSON: {
+            transform: function (doc, ret) {
+                baseUserTransform(doc, ret);
+                return ret;
+            },
+        },
+    },
+);
+
+climberSchema.methods.editUser = async function (updates) {};
+
+const Climber = User.discriminator("Climber", climberSchema);
+
+module.exports = User;
