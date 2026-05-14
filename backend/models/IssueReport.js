94ee9f17733a615936fc34c16cdcd171e31796b7
Mirko Lana <lana.mirko@icloud.com>
2026-05-14 15:54:08 +0200

    refactor: add code refactor #17

--- backend/models/IssueReport.js
+++ backend/models/IssueReport.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Climber } = require("../models/User");
 const { Wall } = require("../models/Wall");
 
-const reportSchema = new mongoose.Schema({
+const issueReportSchema = new mongoose.Schema({
     climber_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Climber",
@@ -17,8 +17,8 @@ const reportSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true,
-        maxlength: [500, "Report body cannot exceed 500 characters"],
+        maxlength: [500, "Issue report body cannot exceed 500 characters"],
     },
 });
 
-module.exports = mongoose.model("Report", reportSchema);
+module.exports = mongoose.model("IssueReport", issueReportSchema);
